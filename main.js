3818c0cf3285a779356428e690235382ffa48b01
TAKiiiiKAT <110965175+TAKiiiiKAT@users.noreply.github.com>
2022-08-11 10:25:43 +0800

    1.6

--- main.js
+++ main.js
@@ -249,7 +249,7 @@ if (false) {
         'landscape right': 3
     };
     BK.Director.screenMode = ORIENTATIONS[window._CCSettings.orientation];
-    init Adapter();
+    initAdapter();
     cc.game.once(cc.game.EVENT_ENGINE_INITED, function() {
         initRendererAdapter();
     });
